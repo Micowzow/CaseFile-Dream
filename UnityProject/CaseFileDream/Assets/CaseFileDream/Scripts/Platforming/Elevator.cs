5caf1e18bc860745f7f99ecdb5dbf7d4b3dd149d
Micowzow <benmichau@hotmail.com>
2024-03-28 00:21:28 +0800

    Tutorial area and Elevator Tests
    Setting up tutorial level and beginning to create elevator systems for our Chandelier mechanic

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Platforming/Elevator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Transform player;
+    public Transform elevatorSwitch;
+    public Transform downPos;
+    public Transform upperPos;
+
+    public float speed;
+    bool isElevaterDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartElevator();
+        
+    }
+
+    void StartElevator()
+    {
+        if(Vector2.Distance(player.position,elevatorSwitch.position)<0.5f && Input.GetKeyDown("e")) //Change keydown to && torch proximity
+        {
+            if(transform.position.y <= downPos.position.y)
+            {
+                isElevaterDown = true;
+            }
+            else if(transform.position.y >= upperPos.position.y)
+            {
+                isElevaterDown = false;
+            }
+        }
+
+        if (isElevaterDown)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, upperPos.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, downPos.position, speed * Time.deltaTime);
+        }
+    }
+}
