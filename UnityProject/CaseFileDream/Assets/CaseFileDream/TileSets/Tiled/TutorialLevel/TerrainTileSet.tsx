f037b4b6aeeab38293bd3295ce755ae13ba7424d
Micowzow <benmichau@hotmail.com>
2024-04-04 20:20:26 +0800

    Tiled2D TileMaps
    Setting up tile maps with Tiled2D

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/TutorialLevel/TerrainTileSet.tsx
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Terrain" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <image source="../../TileSets/temp_tiles_for_quick_test.png" width="224" height="160"/>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="32" height="30"/>
+   <object id="2" x="0" y="2" width="32" height="30"/>
+   <object id="3" x="0" y="2" width="32" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0909091" width="32" height="31.9091"/>
+   <object id="2" x="0" y="1" width="32" height="31"/>
+   <object id="3" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="5" width="25" height="27"/>
+   <object id="2" x="0" y="5" width="25" height="27"/>
+   <object id="3" x="0" y="5" width="25" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="0" width="30" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="27" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="26" height="29"/>
+  </objectgroup>
+ </tile>
+</tileset>
