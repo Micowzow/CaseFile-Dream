9dd7c0c9ed3a6819972a91e079c7d3e78bb833a3
Ben Michau <144104815+Micowzow@users.noreply.github.com>
2024-04-09 19:26:45 +1000

    LanternChagre
    Start Lantern charge system

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Lantern/LanternInteraction.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternInteraction : MonoBehaviour
+{
+    public int fullCharge = 4; 
+    public int currentCharge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCharge = fullCharge;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UseCharge(int amount)
+    {
+        currentCharge -= amount;
+
+        if(currentCharge <= 0)
+        {
+            //No charge can be used
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "LanternInteract")
+        {
+            UseCharge(1);
+        }
+    }
+}
