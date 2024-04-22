c29271ab21d679c2370e618645ba6a3a9240c813
Micowzow <benmichau@hotmail.com>
2024-04-22 22:46:55 +0800

    respawn update

--- UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Enemies/EnemyController.cs
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Enemies/EnemyController.cs
@@ -14,6 +14,8 @@ public class EnemyController : MonoBehaviour
     private Transform currentPoint;
     public float speed;
 
+   
+
     // Start is called before the first frame update
     void Start()
     {
