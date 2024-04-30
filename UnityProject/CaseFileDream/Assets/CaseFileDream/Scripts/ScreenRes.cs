5c4e533158e4f732cbfbdac7a361506fba8d9e79
Ben Michau <144104815+Micowzow@users.noreply.github.com>
2024-04-30 13:49:35 +0800

    AddedAssets

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/ScreenRes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRes : MonoBehaviour
+{
+    void Start()
+    {
+        print(Screen.currentResolution);
+    }
+}
