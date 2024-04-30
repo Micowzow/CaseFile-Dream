1338045205ed3913449c4c9755e725b51426b511
Ben Michau <144104815+Micowzow@users.noreply.github.com>
2024-04-30 15:27:54 +0800

    Menus and Timer

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/TimerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Image timeRadial;
+    public Respawning respawning;
+    public PauseMenu pauseMenu;
+
+    public float timeRemaining;
+    public float maxTime = 5.0f;
+
+    public void Awake() 
+    {
+        
+    }
+    void Start()
+    {
+        timeRemaining = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            timeRadial.fillAmount = timeRemaining / maxTime;
+        }
+        else
+        {
+            respawning.Die();
+            pauseMenu.LoadMenu();
+        }
+    }
+}
