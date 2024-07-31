b4b011c211d367f550062c013801799c9464da1a
Micowzow <benmichau@hotmail.com>
2024-07-31 13:35:12 +0800

    Remove Input System

--- UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Enemies/EnemyHealthManager.cs
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Enemies/EnemyHealthManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using XInputDotNetPure;
-using UnityEngine.InputSystem;
 
 
 public class EnemyHealthManager : MonoBehaviour
