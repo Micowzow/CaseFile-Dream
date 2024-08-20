1ec548ebefefae08e160bc945fbed0c45cfd1cbd
GranolaCrocs <163104465+GranolaCrocs@users.noreply.github.com>
2024-08-20 19:43:24 +0800

    Fixed Tilesets and Cathedral map import
    there was an issue with the cathedral tiles texture and the import settings, together they caused that TV static error
    also, turns out you cant have both a perspective cam and the pixel perfect cam active

--- UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/Cathedral/CathedralGround.tsx
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/Cathedral/CathedralGround.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="CathedralGround" tilewidth="32" tileheight="32" tilecount="56" columns="8">
+<tileset version="1.9" tiledversion="1.9.2" name="CathedralGround" tilewidth="32" tileheight="32" tilecount="56" columns="8">
  <image source="../../TileSets/tilesetCathedralGround32-32.png" width="256" height="225"/>
  <tile id="8">
   <objectgroup draworder="index" id="3">
@@ -10,27 +10,27 @@
  </tile>
  <tile id="9">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="10">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="11">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="12">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="13">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="14">
@@ -72,6 +72,20 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.272727" y="0">
+    <polygon points="0,0 32.1818,31.9091 0.272727,32.1818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.181818" y="31.9091">
+    <polygon points="0,0 32,-32.1818 32.1818,0"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="25">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
@@ -97,34 +111,41 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="-0.0909091">
+    <polygon points="0,0 0,32.1818 32,32.1818"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="40">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="41">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="43">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="44">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="46">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="47">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
