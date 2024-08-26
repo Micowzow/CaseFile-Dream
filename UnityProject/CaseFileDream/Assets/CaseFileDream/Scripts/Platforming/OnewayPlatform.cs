a87eec7656e6681aae5c91b9833eae7f8c63da99
Ben Michau <144104815+Micowzow@users.noreply.github.com>
2024-08-26 18:04:13 +0800

    One way platform

--- UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Platforming/OnewayPlatform.cs
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Platforming/OnewayPlatform.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class OnewayPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject currentOneWayPlatform;
 
-    // Update is called once per frame
+    public Collider2D playerCollider;
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if( collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        if( collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+
     }
 }
