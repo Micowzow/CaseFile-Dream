501df6af7001e637f7d5ec75375cae732ae4730e
Micowzow <benmichau@hotmail.com>
2024-08-30 19:29:36 +0800

    FixingStuff

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/OnewayPlatform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnewayPlatform : MonoBehaviour
+{
+    private GameObject currentOneWayPlatform;
+
+    public Collider2D playerCollider;
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") == -1)
+        {
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if( collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        if( collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+
+    }
+}
