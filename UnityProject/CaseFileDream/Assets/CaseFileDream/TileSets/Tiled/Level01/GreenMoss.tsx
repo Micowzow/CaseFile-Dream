29c7615c9a3af2310e45ce31da912649eb1ccf9b
Xinran Zhou <163103363+Xinran2333@users.noreply.github.com>
2024-10-07 11:19:17 +0800

    garden overlay tile done
    done

--- UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/Level01/GreenMoss.tsx
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/Level01/GreenMoss.tsx
@@ -61,4 +61,24 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
