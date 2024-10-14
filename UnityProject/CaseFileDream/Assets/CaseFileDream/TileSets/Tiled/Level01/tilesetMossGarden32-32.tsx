4f23a1450d1f03e8009072412a8a692d4937ba8f
fancylecttuce <62030280+fancylecttuce@users.noreply.github.com>
2024-10-14 22:58:10 +0800

    Tilemap Garden

---
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/TileSets/Tiled/Level01/tilesetMossGarden32-32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tilesetMossGarden32-32" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="../../TileSets/tilesetMossGarden32-32.png" width="514" height="312"/>
+</tileset>
