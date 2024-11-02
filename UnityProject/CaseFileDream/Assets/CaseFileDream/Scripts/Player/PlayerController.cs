658eb42d235892c481a9f95035261be103dc9233
Micowzow <benmichau@hotmail.com>
2024-11-02 19:21:10 +0800

    CleanUp

--- UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Player/PlayerController.cs
+++ UnityProject/CaseFileDream/Assets/CaseFileDream/Scripts/Player/PlayerController.cs
@@ -169,7 +169,7 @@ namespace PlayerController
             coyoteUsable = false; //Coyote Jump is not useable
             frameVelocity.y = stats.JumpPower; //Frame movement 
             Jumped?.Invoke(); // Is jump command executed?
-            FindObjectOfType<AudioManager>().Play("Jump");
+            //FindObjectOfType<AudioManager>().Play("Jump");
 
         }
 
